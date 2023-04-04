06e1fb0b86e346cf78274374cd035a9c252e8b1a
Nirnay Behera <nirnay.1behera@gmail.com>
2023-04-04 23:58:52 +0530

    added players and balance

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import lottery from "./lottery";
 function App() {
   const [manager, setManager] = useState("");
   const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       const manager = await lottery.methods.manager().call();
-      const players = await lottery.methods.players().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
 
       setManager(manager);
       setPlayers(players);
+      setBalance(balance);
     }
     fetchData();
   }, []);
@@ -21,7 +24,9 @@ function App() {
     <div>
       <h2>Lottery Contract</h2>
       <p>
-        This contract is managed by {manager}.
+        This contract is managed by {manager}. There are currently{" "}
+        {players.length} people entered, competing to win{" "}
+        {web3.utils.fromWei(balance, "ether")} ether!
       </p>
     </div>
   );
