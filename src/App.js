ec89af51b3b3ecb13482535302a6fbadda662f9a
Nirnay Behera <nirnay.1behera@gmail.com>
2023-04-05 00:11:10 +0530

    added form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import web3 from "./web3/web3";
 import lottery from "./lottery";
- 
+
 function App() {
   const [manager, setManager] = useState("");
   const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState("");
+  const [value, setValue] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -28,8 +29,20 @@ function App() {
         {players.length} people entered, competing to win{" "}
         {web3.utils.fromWei(balance, "ether")} ether!
       </p>
+      <hr></hr>
+      <form>
+        <h2>Try your Luck!!</h2>
+        <div>
+          <label>Type the amount of ether to enter: </label>
+          <input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+          <button>Enter</button>
+        </div>
+      </form>
     </div>
   );
 }
- 
+
 export default App;
